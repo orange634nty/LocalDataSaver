807c606a03665a00e16927e078eec81a34adde65
orange634nty <orange634nty@gmail.com>
2020-12-28 03:10:44 +0900

    initalize commit

---
+++ Assets/LocalDataSaver/Scripts/AbstractLocalData.cs
@@ -0,0 +1,39 @@
+﻿namespace LocalDataSaver
+{
+    public abstract class AbstractLocalData<T> where T : new()
+    {
+        /// <summary>
+        /// Try to load data from local storage
+        /// if file is not exist, create new instance and return
+        /// </summary>
+        /// <returns></returns>
+        public static T Load()
+        {
+            try
+            {
+                return LocalFileHandler<T>.Load();
+            }
+            catch
+            {
+                return new T();
+            }
+        }
+
+        /// <summary>
+        /// Save current data to local storage
+        /// </summary>
+        public void Save()
+        {
+            LocalFileHandler<T>.Save(this);
+        }
+
+        /// <summary>
+        /// initialize data
+        /// </summary>
+        public static void InitializeData()
+        {
+            var data = new T();
+            LocalFileHandler<T>.Save(data);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+LocalDataSaver
+---
+
+this project is work in progress
+
